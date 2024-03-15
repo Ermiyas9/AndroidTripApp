fecdd6e4e7e8611f570d3b9ba0ae11e2b21a722b
Ermias Gulti <Egulti4128@conestogac.on.ca>
2024-03-14 22:06:05 -0400

    Planning to add a RecyclerView after the last page of my trip app. Additionally, we will use this ListView to display food items.

---
+++ app/src/main/java/com/example/tripplannerapp/foodMenuRecycleView.java
@@ -0,0 +1,13 @@
+package com.example.tripplannerapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class foodMenuRecycleView extends AppCompatActivity {
+
+    protected  void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((R.layout.));
+    }
+}
